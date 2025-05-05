19ab9972f5847d78c26b015fb4861d66fcbad0cb
Oksana <oksana@MacBook-Pro-Oksana.local>
2025-05-05 10:56:03 +0200

    updates for logger tests

--- lesson_10/test_homework_10.py
+++ lesson_10/test_homework_10.py
@@ -2,30 +2,46 @@ from lesson_10.homework_10 import log_event
 
 import pytest
 
-def test_log_success():
-    username = "Oksana"
-    status = "success"
-    log_event(username=username, status=status)
-    with open("login_system.log", "r") as file:
-        log_contents = file.read()
+class TestLogEvent:
+    def parse_last_log_line(self):
+        with open("login_system.log", "r") as file:
+            last_line = file.readlines()[-1]
 
-    assert f"Login event - Username: {username}, Status: {status}" in log_contents
+        message_part = last_line.split(" - ")[-1]
+        parts = message_part.strip().split(", ")
+        username = parts[0].split(": ")[-1]
+        status = parts[1].split(": ")[-1]
+        return username, status
 
 
-def test_log_expired():
-    username = "Oksana"
-    status = "expired"
-    log_event(username=username, status=status)
-    with open("login_system.log", "r") as file:
-        log_contents = file.read()
+    def test_log_success(self):
+        username = "Oksana"
+        status = "success"
+        log_event(username=username, status=status)
 
-    assert f"Login event - Username: {username}, Status: {status}" in log_contents
+        actual_username, actual_status = self.parse_last_log_line()
 
-def test_log_failed():
-    username = "Oksana"
-    status = "failed"
-    log_event(username=username, status=status)
-    with open("login_system.log", "r") as file:
-        log_contents = file.read()
+        assert actual_username == username
+        assert actual_status == status
 
-    assert f"Login event - Username: {username}, Status: {status}" in log_contents
+
+    def test_log_expired(self):
+        username = "Oksana"
+        status = "expired"
+        log_event(username=username, status=status)
+
+        actual_username, actual_status = self.parse_last_log_line()
+
+        assert actual_username == username
+        assert actual_status == status
+
+
+    def test_log_failed(self):
+        username = "Oksana"
+        status = "failed"
+        log_event(username=username, status=status)
+
+        actual_username, actual_status = self.parse_last_log_line()
+
+        assert actual_username == username
+        assert actual_status == status
